d3f1d36b6c3e86987281eb76eb14bc91d12d4db0
Emmanuelawe51 <126803425+Emmanuelawe51@users.noreply.github.com>
2024-02-23 12:51:31 +0000

    Added in Atoms
    Atoms are now displayed when user taps on a hexagon

--- SWEProject/src/main/java/com/example/sweproject/GameLauncher.java
+++ SWEProject/src/main/java/com/example/sweproject/GameLauncher.java
@@ -3,7 +3,13 @@ package com.example.sweproject;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
@@ -18,6 +24,10 @@ public class GameLauncher extends Application {
         Stage stage = new Stage();
         Group root = new Group();
         Scene scene = new Scene(root, Color.BLACK);
+        // this is the icon for the window of the game, you may need to change the source of the image location as this
+        // was the only way I could get it to work rn
+        Image icon = new Image("C:\\Users\\emman\\OneDrive\\Documents\\GitHub\\Software-Engineering-Projec\\SWEProject\\src\\main\\java\\com\\example\\sweproject\\MainIcon.png");
+        stage.getIcons().add(icon);
         //side length and height of hexagon
         double side = 50;
         double height = side * Math.sqrt(3);
@@ -57,6 +67,8 @@ public class GameLauncher extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    // counter for adding Atoms
+    private int AtomCount = 0;
 
     //my class to create the hexagons taking in the 3 parameters
     private Polygon createHexagon(double x, double y, double side) {
@@ -77,6 +89,25 @@ public class GameLauncher extends Application {
         // This line sets the outline color of the hexagon to yellow
         hexagon.setStroke(Color.YELLOW);
 
+        // Add a click event handler to the hexagon
+        hexagon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            // Only add an Atom if there are less than 6
+            if (AtomCount < 6) {
+                // sets the positioning of the atom to the centre of the hexagon
+                Circle Atom = new Circle(x, y - side, side / 2);
+                // coloring of the Atom
+                RadialGradient gradient = new RadialGradient(
+                        0, 0, 0.5, 0.5, 0.5, true, CycleMethod.NO_CYCLE,
+                        new Stop(0, Color.RED),
+                        new Stop(1, Color.DARKRED)
+                );
+                Atom.setFill(gradient);
+                // Add the Atom to the same parent as the hexagon
+                ((Group) hexagon.getParent()).getChildren().add(Atom);
+                // Increment the Atom counter
+                AtomCount++;
+            }
+        });
         return hexagon;
     }
 }
